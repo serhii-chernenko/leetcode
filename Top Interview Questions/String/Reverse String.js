d7f99e0e3926589e2878abd03cdd64df3aa4da59
Serhii Chernenko <serhii.c@overdose.digital>
2021-08-14 18:09:47 +0300

    Reverse String added

---
+++ Top Interview Questions/String/Reverse String.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    return s.reverse();
+};
+
+console.log(reverseString(['h', 'e', 'l', 'l', 'o'])); // ['o', 'l', 'l', 'e', 'h']
+console.log(reverseString(['H', 'a', 'n', 'n', 'a', 'h'])); // ['h', 'a', 'n', 'n', 'a', 'H']
